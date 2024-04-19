d307b8069964e8fee5c3f2c19e726d6327d36290
Luiz Guilherme S Seixas <guilhermesschiavon@gmail.com>
2024-04-19 18:42:42 -0300

    Implementa classe auth

--- src/utils/Auth.ts
+++ src/utils/Auth.ts
@@ -1,1 +1,76 @@
-export default class Auth {}
+import { createStorage } from './storage'
+import { type IStorage } from '@/interfaces/IStorage'
+
+export default class Auth {
+  private storage: IStorage
+
+  constructor(persistent = false) {
+    this.storage = createStorage(persistent)
+  }
+
+  private getFallback(key: string): string | null {
+    const transient = createStorage(false)
+    const persistent = createStorage(true)
+    return transient.get(key) || persistent.get(key)
+  }
+
+  async success(response: Response, onSuccess: () => void) {
+    const data = await response.json()
+
+    this.storage.store('token', data.token)
+    this.storage.store('email', data.email)
+
+    onSuccess()
+  }
+
+  failure(response: Response, onFailure: () => void) {
+    onFailure()
+  }
+
+  isLoggedIn() {
+    return Boolean(this.getFallback('token'))
+  }
+
+  currentUser() {
+    if (!this.isLoggedIn()) {
+      return null
+    }
+    return {
+      email: this.getFallback('email')
+    }
+  }
+
+  signOut(andThen = () => {}) {
+    const transient = createStorage(false)
+    const persistent = createStorage(true)
+    transient.remove('token')
+    transient.remove('email')
+    persistent.remove('token')
+    persistent.remove('email')
+    andThen()
+  }
+
+  async signIn(email: string, password: string, onSuccess: () => void, onFailure: () => void) {
+    const body = {
+      login: {
+        email,
+        password
+      }
+    }
+
+    const response = await fetch('http://localhost:3000/sign_in', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    })
+
+    if (response.ok) {
+      this.success(response, onSuccess)
+    } else {
+      this.failure(response, onFailure)
+    }
+  }
+}
